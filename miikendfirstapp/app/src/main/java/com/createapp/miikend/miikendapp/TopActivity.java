3a3c8e86212b3a852c39c87cefffc04fc8d7af5f
miku kanade <miku kanade>
2016-07-24 22:16:55 +0900

    Play画面追加してTOP画面から遷移させるとこまで完了

--- miikendfirstapp/app/src/main/java/com/createapp/miikend/miikendapp/TopActivity.java
+++ miikendfirstapp/app/src/main/java/com/createapp/miikend/miikendapp/TopActivity.java
@@ -1,13 +1,28 @@
 package com.createapp.miikend.miikendapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class TopActivity extends AppCompatActivity {
+public class TopActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
+
+        //スタートボタンにOnClickListenerを付与
+        Button start = (Button)findViewById(R.id.start);
+        start.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.start) {
+            //ゲーム画面起動
+            Intent intent = new Intent(getApplicationContext(), PlayActivity.class);
+            startActivity(intent);
+        }
     }
 }
